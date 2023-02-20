8f1f1fba3caee7e4db4853819384e56a3f7a4c33
AbiollaghJames <abiollajames@protonmail.com>
2023-02-20 10:54:40 -0800

    Header File

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+#include <dirent.h>
+
+char *get_command(char *cmd);
+int exec_command(char **args);
+
+#endif
