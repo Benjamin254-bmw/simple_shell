c9e1ad5604a39cfa297c69ecbc2a7496edc9f73c
AbiollaghJames <abiollajames@protonmail.com>
2023-02-21 19:03:51 -0800

    readme file

---
+++ built_in.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * exit - exits  shell
+ * @args: arr of strs
+ * Return: 0
+ */
+void _exit(char **args)
+{
+	int s;
+
+	if ((_strcmp("exit", args[0]) == 0))
+	{
+		if (args[1])
+			s = atoi(args[1]);
+		free(args[0];
+		free(args);
+		exit(s);
+	}
+}
